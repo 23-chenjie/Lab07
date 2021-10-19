14b85711dc3e336971025e989a729232d69935a8
asdfghjklmum <chenqi63656@gmail.com>
2021-10-19 16:18:14 +0800

    player now goes up when spacebar pressed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
+    Rigidbody rigidbody;
+    float speed = 5f;
 
     void Start()
     {
+        rigidbody = GetComponent<Rigidbody>();
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
     }
@@ -17,6 +21,15 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             thisAnimation.Play();
+            rigidbody.velocity = transform.up * speed;
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameOver");
         }
     }
 }
